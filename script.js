593120023863a9ddaed14cc97ed72f1124fc68b6
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-01-29 17:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	n=str.length;
+	for(let i=0; i<n;i++){
+		if (str.charAt(i) === str.charAt(i + 1)) {
+			i++; 
+		}else{
+			return str.charAt(i);
+		}
+	} 
+	return null; 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
